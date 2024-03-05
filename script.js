672f6da38583acaa4856c2f488f734d1a362360d
Muhammad Toni Berlindo <berlindoindo2@gmail.com>
2024-03-05 16:11:55 +0700

    komit terooooossssss

--- script.js
+++ script.js
@@ -1,33 +1,33 @@
 const accessKey = "AMvuCRXEmNFQJ1LH7y9cCijWtrppwkYz8kr1kH89r-8";
 
-const formEl= document.querySelector("form");
+const formEl = document.querySelector("form");
 const inputEl = document.getElementById("Search-input");
 const SearchResults = document.querySelector(".Search-result");
-const showMore = document.getElementById("show-more-button");
+const showMoreButton = document.getElementById("show-more-button");
 
 let inputData = "";
 let page = 1;
 
-async function searchImages(){
+async function searchImages() {
     inputData = inputEl.value;
-    const url = 'https://api.unsplash.com/search/photos?page=${page}&query=${inputData}&client_id=${accessKey}';
+    const url = `https://api.unsplash.com/search/photos?page=${page}&query=${inputData}&client_id=${accessKey}`;
 
     const response = await fetch(url);
     const data = await response.json();
 
     const results = data.results;
 
-    if (page === 1){
+    if (page === 1) {
         SearchResults.innerHTML = "";
     }
 
-    results.map((result) =>{
+    results.forEach((result) => {
         const imageWrapper = document.createElement("div");
         imageWrapper.classList.add("Search-result");
         const image = document.createElement("img");
         image.src = result.urls.small;
         image.alt = result.alt_description;
-        const imageLink = Document.createElement("a");
+        const imageLink = document.createElement("a");
         imageLink.href = result.links.html;
         imageLink.target = "_blank";
         imageLink.textContent = result.alt_description;
@@ -39,17 +39,19 @@ async function searchImages(){
 
     page++;
 
-    if(page > 1){
-        showMore.style.display = "block";
+    if (results.length > 0) {
+        showMoreButton.style.display = "block";
+    } else {
+        showMoreButton.style.display = "none";
     }
 }
 
-formEl.addEventListener("submit", (event) =>{
+formEl.addEventListener("submit", (event) => {
     event.preventDefault();
     page = 1;
     searchImages();
-})
+});
 
-showMore.addEventListener("click", () =>{
+showMoreButton.addEventListener("click", () => {
     searchImages();
-})
+});
